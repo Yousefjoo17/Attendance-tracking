0bbb16aae0ad2f1d6b0b6762d2d95188bc338df7
Youssef <yousefyousefyousef335@gmail.com>
2023-09-20 19:58:46 +0300

    defining firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:attendence_tracking/constants.dart';
+import 'package:attendence_tracking/firebase_options.dart';
 import 'package:attendence_tracking/views/Login_View.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AttendenceTrackingApp());
 }
 

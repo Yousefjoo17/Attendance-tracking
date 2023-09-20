55d590abd71f5251079e2d1cf85c447017d113b7
Youssef <yousefyousefyousef335@gmail.com>
2023-09-20 19:33:01 +0300

    show Snack Bar

---
+++ lib/helpers/Show_snack_bar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void showmySnackBar(BuildContext context, String message,
+    {Duration duration = const Duration(seconds: 3)}) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(message),
+      duration: duration,
+    ),
+  );
+}
